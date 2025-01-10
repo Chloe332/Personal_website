4af1c0221bd94f8872420b6ee8dccc1ba368253b
Chloe Hallaert <cxhallae@uwaterloo.ca>
2025-01-10 00:51:37 -0500

    Initial commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,8 @@
 'use strict';
+ 
 
-
-
-// element toggle function
+// element toggle function 
 const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
-
 
 
 // sidebar variables
@@ -17,6 +15,7 @@ sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); }
 
 
 // testimonials variables
+
 const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
 const modalContainer = document.querySelector("[data-modal-container]");
 const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
